bdc0e119f4e3540b5faadabc9d9462e17c3335d6
Simon Gustavsson <simgus1234@gmail.com>
2023-12-03 18:37:51 +0100

    Laying groundwork for new portfolio page

---
+++ script.js
@@ -0,0 +1,37 @@
+let images = [
+    document.getElementById('image1'),
+    document.getElementById('image2'),
+    document.getElementById('image3'),
+    document.getElementById('image4'),
+    document.getElementById('image5'),
+    document.getElementById('image6'),
+    document.getElementById('image7'),
+    document.getElementById('image8'),
+    document.getElementById('image9'),
+    document.getElementById('image10'),
+    document.getElementById('image11'),
+    document.getElementById('image12'),
+    document.getElementById('image13'),
+    document.getElementById('image14'),
+    document.getElementById('image15'),
+    document.getElementById('image16'),
+    document.getElementById('image17'),
+    document.getElementById('image18'),
+    document.getElementById('logostuff')
+
+];
+
+const parallax = (() => {
+
+        window.addEventListener("scroll", function(){
+            let scroll = window.scrollY;
+
+            let layerbox = document.getElementsByClassName("parallax");
+            let layer, 
+
+            console.log(layerbox)
+
+        });
+
+})();
+
